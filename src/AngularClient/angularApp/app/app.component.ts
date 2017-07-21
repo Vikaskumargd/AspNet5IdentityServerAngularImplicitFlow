55a5cad95c9e9c22aa101265d0b7a28c8a3ae436
damienbod <damien_bod@hotmail.com>
2017-07-21 14:50:28 +0200

    updating to new version of oidc

--- src/AngularClient/angularApp/app/app.component.ts
+++ src/AngularClient/angularApp/app/app.component.ts
@@ -1,6 +1,6 @@
-﻿import { Component, OnInit } from '@angular/core';
+﻿import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
-
+import { Subscription } from 'rxjs/Subscription';
 import { Configuration } from './app.constants';
 
 import { OidcSecurityService } from 'angular-auth-oidc-client';
@@ -24,15 +24,27 @@ import './app.component.css';
     templateUrl: 'app.component.html'
 })
 
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
+
+    isAuthorizedSubscription: Subscription;
+    isAuthorized: boolean;
 
     constructor(public oidcSecurityService: OidcSecurityService) {
     }
 
     ngOnInit() {
+        this.isAuthorizedSubscription = this.oidcSecurityService.getIsAuthorized().subscribe(
+            (isAuthorized: boolean) => {
+                this.isAuthorized = isAuthorized;
+            });
+
         if (window.location.hash) {
             this.oidcSecurityService.authorizedCallback();
         }
+    }
+
+    ngOnDestroy(): void {
+        this.isAuthorizedSubscription.unsubscribe();
     }
 
     login() {
