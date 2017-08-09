7d4432f3482fc81f16319239ae56cda43126899a
damienbod <damien_bod@hotmail.com>
2017-08-09 22:06:04 +0200

    updating to 1.3.3

--- src/ResourceWithIdentityServerWithClient/angularApp/app/user-management/UserManagementService.ts
+++ src/ResourceWithIdentityServerWithClient/angularApp/app/user-management/UserManagementService.ts
@@ -3,7 +3,7 @@ import { Http, Response, Headers, RequestOptions } from '@angular/http';
 import 'rxjs/add/operator/map';
 import { Observable } from 'rxjs/Observable';
 import { Configuration } from '../app.constants';
-import { OidcSecurityService } from 'angular-auth-oidc-client';
+import { OidcSecurityService } from '../auth/services/oidc.security.service';
 import { User } from './models/User';
 
 @Injectable()
